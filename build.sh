d41dd8c5675eff38a483a29ff298c00e1160ca1a
Jannis R <mail@jannisr.de>
2020-10-18 17:50:38 +0200

    download & extract pev2

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+curl -L -s -O 'https://github.com/dalibo/pev2/releases/download/v0.14.1/pev2.tar.gz'
+
+rm -rf lib/*
+tar -x -C lib -f pev2.tar.gz --strip-components 1
