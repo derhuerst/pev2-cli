f09a08bfc4c93305ac4f0570d16e12e2c5776776
Jannis R <mail@jannisr.de>
2021-12-24 12:08:31 +0100

    build with pev2@0.24.0, 1.11.0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-curl -L -s -O 'https://github.com/dalibo/pev2/releases/download/v0.23.0/pev2.tar.gz'
+curl -L -s -O 'https://github.com/dalibo/pev2/releases/download/v0.24.0/pev2.tar.gz'
 
 rm -rf lib/*
 tar -x -C lib -f pev2.tar.gz --strip-components 1
