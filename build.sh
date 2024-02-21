963439c334ddc95e3dd99ac1b0cc8d62d3bf93bb
Jannis R <mail@jannisr.de>
2024-02-21 11:46:10 +0100

    use pev2@1.9.1; 1.22.0

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-curl -fsSL -o lib/index.html 'https://github.com/dalibo/pev2/releases/download/v1.8.0/index.html'
+curl -fsSL -o lib/index.html 'https://github.com/dalibo/pev2/releases/download/v1.9.1/index.html'
