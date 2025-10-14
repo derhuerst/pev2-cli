4681939d8fe0f456d2a7b70238a0f76f547d638d
Jannis R <mail@jannisr.de>
2025-10-14 13:34:18 +0200

    mri -> util.parseArgs

--- cli.js
+++ cli.js
@@ -5,48 +5,74 @@
 import {createRequire} from 'module';
 const require = createRequire(import.meta.url);
 
-import mri from 'mri'
+import {parseArgs} from 'node:util'
 const pkg = require('./package.json')
 
-const argv = mri(process.argv.slice(2), {
-	boolean: [
-		'help', 'h',
-		'version', 'v',
-		'open', 'o',
-		'quiet', 'q',
-		'once', '1',
-	]
+const {
+	values: flags,
+	positionals: args,
+} = parseArgs({
+	options: {
+		'help': {
+			type: 'boolean',
+			short: 'h',
+		},
+		'version': {
+			type: 'boolean',
+			short: 'v',
+		},
+		'open': {
+			type: 'boolean',
+			short: 'o',
+		},
+		'app': {
+			type: 'string',
+		},
+		'quiet': {
+			type: 'boolean',
+			short: 'q',
+		},
+		'once': {
+			type: 'boolean',
+			short: '1',
+		},
+		'name': {
+			type: 'string',
+			short: 'n',
+		},
+	},
+	allowPositionals: true,
 })
 
-if (argv.help || argv.h) {
+if (flags.help) {
 	process.stdout.write(`
 Usage:
-    pev2 <path-to-query-file> [path-to-execution-plan]
+	pev2 <path-to-query-file> [path-to-execution-plan]
 Notes:
-    This tool uses the pev2 to visualize PostgreSQL's execution plan including
-    costs associated to each operation.
-        more info: https://www.postgresql.org/docs/14/using-explain.html
-    To let PostgreSQL generate the plan, prefix it with the following line:
-        EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
+	This tool uses the pev2 to visualize PostgreSQL's execution plan including
+	costs associated to each operation.
+		more info: https://www.postgresql.org/docs/14/using-explain.html
+	To let PostgreSQL generate the plan, prefix it with the following line:
+		EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
 
-    If you don't pass an execution plan, this tool will spawn \`psql\` to run the
-    the query. Use the \`PG*\` environment variables to make sure \`psql\` can
-    connect to the right database.
-    	more info: https://www.postgresql.org/docs/14/libpq-envars.html
+	If you don't pass an execution plan, this tool will spawn \`psql\` to run the
+	the query. Use the \`PG*\` environment variables to make sure \`psql\` can
+	connect to the right database.
+		more info: https://www.postgresql.org/docs/14/libpq-envars.html
 Options:
-    --open     -o  Open the URL in the browser.
-    --app          The browser to open the URL with.
-    --quiet    -q  Don't report what's going on.
-    --once     -1  Stop serving after pev2 has received the data.
-    --name     -n  Give the execution plan a name within pev2.
-                     Default: filename and ISO date+time
+	--open     -o  Open the URL in the browser.
+	--app          The browser to open the URL with.
+	--quiet    -q  Don't report what's going on.
+	--once     -1  Stop serving after pev2 has received the data.
+	--name     -n  Give the execution plan a name within pev2.
+					 Default: filename and ISO date+time
 Examples:
-    pev2 --open --app firefox -q path/to/some-explain-query.sql
+	pev2 --open --app firefox -q path/to/some-explain-query.sql
 \n`)
 	process.exit(0)
 }
 
-if (argv.version || argv.v) {
+if (flags.version) {
 	process.stdout.write(`${pkg.name} v${pkg.version}\n`)
 	process.exit(0)
 }
@@ -63,19 +89,19 @@ const showError = (err) => {
 }
 
 try {
-	const pathToQuery = argv._[0]
+	const pathToQuery = args[0]
 	if (!pathToQuery) {
 		showError('Missing 1st argument: path to EXPLAIN query file.')
 	}
 	const query = readFileSync(pathToQuery, {encoding: 'utf8'})
 
-	const pathToExecPlan = argv._[1] || null
+	const pathToExecPlan = args[1] || null
 
-	const quiet = !!(argv.quiet || argv.q)
-	const once = !!(argv.once || argv['1'])
+	const quiet = !!flags.quiet
+	const once = !!(flags.once || flags['1'])
 
-	const filename = argv._[1] ? basename(pathToExecPlan) : basename(pathToQuery)
-	const name = argv.name || argv.n || `${filename} ${new Date().toISOString()}`
+	const filename = args[1] ? basename(pathToExecPlan) : basename(pathToQuery)
+	const name = flags.name || `${filename} ${new Date().toISOString()}`
 
 	let explainResult = null
 	if (pathToExecPlan !== null) {
@@ -95,10 +121,10 @@ try {
 		name,
 	})
 	if (!quiet) console.info(`serving pev2 at ${url}`)
-	if (argv.open || argv.o) {
+	if (flags.open) {
 		await open(url, {
-			...(argv.app ? {
-				app: {name: argv.app},
+			...(flags.app ? {
+				app: {name: flags.app},
 			} : {}),
 		})
 	}
