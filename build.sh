69662bffa0caf16cd042b2377cf71d30ff706304
Jannis R <mail@jannisr.de>
2026-07-20 10:56:31 +0200

    use pev2@1.23.0; 2.12.0

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 # see also https://github.com/dalibo/pev2/issues/292#issuecomment-2721487239
-curl -fsSL -o lib/index.html 'https://github.com/dalibo/pev2/releases/download/v1.22.0/pev2.html'
+curl -fsSL -o lib/index.html 'https://github.com/dalibo/pev2/releases/download/v1.23.0/pev2.html'
